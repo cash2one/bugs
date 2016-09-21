0d9ea76602cad2e62d0bcf23d27989f88d9e04f7
cywxf <cheny911@163.com>
2016-09-21 22:01:20 +0800

    use matplotlib to draw the value trend graph

--- stocklist.py
+++ stocklist.py
@@ -5,18 +5,22 @@ from pandas.io import data
 import matplotlib.pyplot as plt
 
 stockcode = ["000881.sz","600802.ss","600200.ss"]
-beginday  = "2016/01/01"
+beginday  = "2004/01/01"
 
 for i in stockcode:
     #print "\n" , i
     stockvalue = pandas.io.data.DataReader(i,"yahoo", start=beginday)
     #print stockvalue.tail(5)
-    plt.plot(stockvalue.index, stockvalue["Close"])
+    plot = plt.plot(stockvalue.index, stockvalue["Close"])
+#    plt.legend(plot, i)
     
     #print stockvalue.tail(-1)
     #print stockvalue.index
     #print stockvalue["Close"].tail(1)
     #print type(stockvalue["Close"])
 
+plt.title("stock value graph")
+plt.xlabel("Date")
+plt.ylabel("Value")
 plt.show()
 print "end"

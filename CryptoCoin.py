a675b48e1401dd589aecc5da861923e9ecf3ed0a
cywxf <cheny911@163.com>
2016-09-29 11:56:58 +0900

    Update CryptoCoin.py

--- CryptoCoin.py
+++ CryptoCoin.py
@@ -118,22 +118,7 @@ try:
     # coinDict = r.json()[0]
     # print type(coinDict)
     # print coinDict
-    
-    '''
-            "name": "Bitcoin", 
-            "symbol": "BTC", 
-            "rank": "1", 
-            "price_usd": "573.137", 
-            "price_btc": "1.0", 
-            "24h_volume_usd": "72855700.0", 
-            "market_cap_usd": "9080883500.0", 
-            "available_supply": "15844176.0", 
-            "total_supply": "15844176.0", 
-            "percent_change_1h": "0.04", 
-            "percent_change_24h": "-0.3", 
-            "percent_change_7d": "-0.57", 
-            "last_updated": "1472762067"
-    '''
+
     print " NO SYMBOL NAME                PRICE   24H_VOLUME     MARKET_CAP    AVAILABLE        TOTAL  CHG_1H CHG_24H  CHG_7D  LAST_UPDATED" 
     print "-"*135
     for coinDict in r.json():
